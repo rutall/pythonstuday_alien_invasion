81e1618b950dfbe656cef28b8926e1ee000a1ff4
rutal <1374253704@qq.com>
2024-07-31 14:22:06 +0800

    基础屏幕模块

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,7 +9,10 @@ class AlienInvasion:
 
         #创建一个长1200，宽800的窗口
         self.screen = pg.display.set_mode((1200, 800))
-
+        #初始化时钟属性
+        self.clock = pg.time.Clock()
+        #设置背景颜色
+        self.bg_color = (230, 230, 230)
     def run_game(self):
         while True:
             #侦听键盘和鼠标事件
@@ -17,8 +20,11 @@ class AlienInvasion:
                 if event.type == pg.QUIT:
                     sys.exit()
 
+            #重新绘制屏幕
+            self.screen.fill(self.bg_color)
             #显示最近绘制的屏幕
             pg.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
